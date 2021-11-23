ba8cbd169fc6ee4de34d1765380964159cbcd65f
yyyyjinying <1020066395@qq.com>
2021-11-23 23:27:22 +0800

    all

--- changgou/changgou-parent/spring-cloud-config-client-test01/src/main/java/com/cloud/controller/ConfigClientController.java
+++ changgou/changgou-parent/spring-cloud-config-client-test01/src/main/java/com/cloud/controller/ConfigClientController.java
@@ -1,10 +1,12 @@
 package com.cloud.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope // 该配置可以手动刷新后生效  curl -X POST "http://localhost:18041/actuator/refresh"
 public class ConfigClientController {
 //    @Value("${aa.bb}")
     @Value("${config.info}")
