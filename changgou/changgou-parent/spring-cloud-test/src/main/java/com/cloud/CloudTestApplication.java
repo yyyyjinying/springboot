d207e62e895141f4ef92b71d23d9f48edbf05799
yyyyjinying <1020066395@qq.com>
2021-12-02 23:18:27 +0800

    all

--- changgou/changgou-parent/spring-cloud-test/src/main/java/com/cloud/CloudTestApplication.java
+++ changgou/changgou-parent/spring-cloud-test/src/main/java/com/cloud/CloudTestApplication.java
@@ -1,10 +1,12 @@
 package com.cloud;
 
+import com.cloud.interceptor.FeignInterceptor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -14,6 +16,15 @@ public class CloudTestApplication {
     public static void main(String[] args) {
 
         SpringApplication.run(CloudTestApplication.class, args);
+    }
+
+    /***
+     * 创建拦截器Bean对象
+     * @return
+     */
+    @Bean
+    FeignInterceptor feignInterceptor(){
+        return new FeignInterceptor();
     }
 
 }
---
+++ changgou/changgou-parent/spring-cloud-test/src/main/java/com/cloud/interceptor/FeignInterceptor.java
@@ -0,0 +1,34 @@
+package com.cloud.interceptor;
+
+import feign.RequestInterceptor;
+import feign.RequestTemplate;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Enumeration;
+
+public class FeignInterceptor implements RequestInterceptor {
+    @Override
+    public void apply(RequestTemplate requestTemplate) {
+        try {
+            ServletRequestAttributes requestAttributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+
+            if (requestAttributes != null) {
+                // 获取所有头文件信息的key
+                HttpServletRequest request = requestAttributes.getRequest();
+                Enumeration<String> headerNames = request.getHeaderNames();
+                if (headerNames != null) {
+                    while (headerNames.hasMoreElements()) {
+                        String key = headerNames.nextElement();
+                        String value = request.getHeader(key);
+                        requestTemplate.header(key, value);
+                    }
+                }
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
